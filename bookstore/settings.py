87f544dd031a9c32dc8830bb249341ffad483049
Wgroliveira <129519069+Wgroliveira@users.noreply.github.com>
2024-12-09 15:20:42 -0300

    Configurando o Pythonanywhere

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-h7fr_hj)gb+c@axs79bk&=r#g-8+q9+j0i7%f!tfq%$iql-20t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ebac-bookstore-api.herokuapp.com', 'wagnerwgr.pythonanywhere.com']
 
 
 # Application definition
